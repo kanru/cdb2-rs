abd9c13dd3dca9a246c4e19eacd5acbcf1223ff0
Kan-Ru Chen <kanru@kanru.info>
2024-01-25 23:29:53 +0900

    refactor: use u32 from_le_bytes and to_le_bytes

--- src/uint32.rs
+++ src/uint32.rs
@@ -1,33 +1,19 @@
 use std::u32;
 
-pub fn unpack(data: &[u8]) -> u32 {
-    assert!(data.len() >= 4);
-    // Use u32::from_bytes when it stabilizes
-    // Rust compiles this down to an efficient word copy
-    (data[0] as u32) | ((data[1] as u32) << 8) | ((data[2] as u32) << 16) | ((data[3] as u32) << 24)
+pub(crate) fn unpack(data: &[u8]) -> u32 {
+    u32::from_le_bytes(data.try_into().unwrap())
 }
 
-pub fn unpack2(buf: &[u8]) -> (u32, u32) {
+pub(crate) fn unpack2(buf: &[u8]) -> (u32, u32) {
+    assert!(buf.len() >= 8);
     (unpack(&buf[0..4]), unpack(&buf[4..8]))
 }
 
-fn _pack(src: u32) -> [u8; 4] {
-    // Use u32::to_bytes when it stabilizes
-    // Rust compiles this down to an efficient word copy
-    [
-        src as u8,
-        (src >> 8) as u8,
-        (src >> 16) as u8,
-        (src >> 24) as u8,
-    ]
+fn pack(data: &mut [u8], src: u32) {
+    data[..4].copy_from_slice(&src.to_le_bytes());
 }
 
-pub fn pack(data: &mut [u8], src: u32) {
-    assert!(data.len() >= 4);
-    data[..4].copy_from_slice(&_pack(src));
-}
-
-pub fn pack2(data: &mut [u8], src0: u32, src1: u32) {
+pub(crate) fn pack2(data: &mut [u8], src0: u32, src1: u32) {
     assert!(data.len() >= 8);
     pack(&mut data[0..4], src0);
     pack(&mut data[4..8], src1);
